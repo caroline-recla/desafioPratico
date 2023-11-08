fea0097e99ee84f5b48765e49971e5fdcf1993c4
Caroline Recla <105314181+caroline-recla@users.noreply.github.com>
2023-11-08 18:55:46 -0300

    Update desafio3.java

--- ModalGRDesafio/desafio3.java
+++ ModalGRDesafio/desafio3.java
@@ -1,55 +1,79 @@
 package ModalGRDesafio;
 import java.util.Scanner;
+
 public class desafio3{
     
     public static void notasMaiorValor(int emprestimo){
         System.out.println("Notas de maior valor: ");
-        int notas100 = emprestimo/100;
-        int notas50 = (emprestimo%100)/50;
-        int notas20 = (emprestimo%50)/20;
-        int notas10 = (emprestimo%20)/10;
+        
+        int notas100 = emprestimo / 100;
+        emprestimo -= notas100 * 100; 
+        
+        int notas50 = (emprestimo%100) / 50;
+        emprestimo -= notas50 * 50; 
+        
+        int notas20 = (emprestimo%50) / 20;
+        emprestimo -= notas20 * 20; 
+        
+        int notas10 = (emprestimo%20) / 10;
+        emprestimo -= notas10 * 10; 
+        
         int notas5 = (emprestimo%10)/5;
+        emprestimo -= notas5 * 5; 
+        
         int notas2 = (emprestimo%5)/2;
-        System.out.println(""+notas100+" X 100 reais;");
-        if(notas50 != 0){
+
+        System.out.println(""+notas100+" X 100 reais.");
+        if(notas50 > 0){
          System.out.println(""+notas50+" X 50 reais.");   
         }
-        if(notas20 != 0){
-          System.out.println(""+notas20+" X 20 reais;");
+        if(notas20 > 0){
+          System.out.println(""+notas20+" X 20 reais.");
         }
-        if(notas10 != 0){
+        if(notas10 > 0){
           System.out.println(""+notas10+" X 10 reais.");  
         }
-        if(notas5 != 0){
+        if(notas5 > 0){
           System.out.println(""+notas5+" X 5 reais.");
         }
-        if(notas2 != 0){
+        if(notas2 > 0){
           System.out.println(""+notas2+" X 2 reais.");
         }
     }
     
     public static void notasMenorValor(int emprestimo){
         System.out.println("Notas de menor valor: ");
-        int notas20 = emprestimo/20;
-        int notas10 = (emprestimo%20)/10;
+
+        int notas20 = (emprestimo%50) / 20;
+        emprestimo -= notas20 * 20; 
+        
+        int notas10 = (emprestimo%20) / 10;
+        emprestimo -= notas10 * 10; 
+        
         int notas5 = (emprestimo%10)/5;
+        emprestimo -= notas5 * 5; 
+        
         int notas2 = (emprestimo%5)/2;
+        
         System.out.println(""+notas20+" X 20 reais;");
-        if(notas10 != 0){
+        if(notas10 > 0){
           System.out.println(""+notas10+" X 10 reais.");  
         }
-        if(notas5 != 0){
+        if(notas5 > 0){
           System.out.println(""+notas5+" X 5 reais.");
         }
-        if(notas2 != 0){
+        if(notas2 > 0){
           System.out.println(""+notas2+" X 2 reais.");
         }
     }
+    
     public static void notasMeioAMeio(int emprestimo){
       int metadeValor = emprestimo/2;
+      
       System.out.println("Notas meio a meio: ");
       System.out.println(metadeValor+" em notas de maior valor");
       notasMaiorValor(metadeValor);
+      System.out.println("");
       System.out.println(metadeValor+" em notas de menor valor");
       notasMenorValor(metadeValor);
     }
@@ -69,6 +93,7 @@ public class desafio3{
     int salario = scan.nextInt();
 
     if(admissao>5){
+      System.out.println("Valor do emprestimo: ");
       int emprestimo = scan.nextInt();
       if(emprestimo > 0 && emprestimo <= 2 * salario && emprestimo % 2 == 0){
           System.out.println("Valor do Empréstimo: \n"+ emprestimo);
